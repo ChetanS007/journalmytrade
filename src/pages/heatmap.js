159959dda6c37bcc20fcce79644e7bca4fb3ae35
tohel17 <tohilchini786@gmail.com>
2023-09-22 18:07:10 +0530

    add  some necessary library

--- src/pages/heatmap.js
+++ src/pages/heatmap.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import CalendarHeatmap from 'reactjs-calendar-heatmap';
-
+import React from "react";
+import CalendarHeatmap from "reactjs-calendar-heatmap";
 
 function Heatmap() {
   // Generate sample data for each day with 'summary'
   const generateData = () => {
     const data = [];
-    const startDate = new Date('2023-01-01'); // Define your start date
-    const endDate = new Date('2023-12-31');   // Define your end date
+    const startDate = new Date("2023-01-01"); // Define your start date
+    const endDate = new Date("2023-12-31"); // Define your end date
 
     let currentDate = new Date(startDate);
 
     while (currentDate <= endDate) {
       data.push({
-        date: currentDate.toISOString().split('T')[0], // Format date as "YYYY-MM-DD"
+        date: currentDate.toISOString().split("T")[0], // Format date as "YYYY-MM-DD"
         count: Math.floor(Math.random() * 10), // Replace with your actual data
-        summary: 'Some summary data', // Include a 'summary' property
+        summary: "Some summary data", // Include a 'summary' property
       });
 
       // Increment the current date by one day
@@ -31,8 +30,8 @@ function Heatmap() {
     <div id="calendar-heatmap">
       <CalendarHeatmap
         values={seriesData}
-        startDate={new Date('2023-01-01')} // Replace with your desired start date
-        endDate={new Date('2023-12-31')}   // Replace with your desired end date
+        startDate={new Date("2023-01-01")} // Replace with your desired start date
+        endDate={new Date("2023-12-31")} // Replace with your desired end date
       />
     </div>
   );
