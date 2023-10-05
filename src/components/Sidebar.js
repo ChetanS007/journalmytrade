8e4f5bb976bbacbd631bab73e62898760eb58d3a
tohel17 <tohilchini786@gmail.com>
2023-10-05 12:12:11 +0530

    removed unused code

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,9 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import homeImg1 from "./../pages/imagesPage/homeImg1.png";
 
 export default function Sidebar() {
-  const [sidebarData, setsidebarData] = React.useState([
-    { item: "Dashboard", to: "/Dashboard", isActive: true },
-    { item: "Journal", to: "/Journal", isActive: false },
-    { item: "Charts", to: "/Charts", isActive: false },
-    { item: "Notes", to: "/Notes", isActive: false },
-    { item: "Accounts", to: "/Accounts", isActive: false },
-    { item: "Risk Management", to: "/RiskManagement", isActive: false },
-  ]);
-
-  function MenuItem({ label, to, isActive }) {
+  function MenuItem({ label, to }) {
     return (
       <NavLink
         to={to}
@@ -30,11 +21,7 @@ export default function Sidebar() {
       <div className="dash-logo">
         <img src={homeImg1} alt="Journal My Trade" class="dashBanner-logo" />
       </div>
-      <MenuItem
-        label="Dashboard"
-        to={"/Dashboard"}
-        isActive={sidebarData[0].isActive}
-      />
+      <MenuItem label="Dashboard" to={"/Dashboard"} />
       <MenuItem label="Journal" to={"/Journal"} />
       <MenuItem label="Charts" to="/Charts" />
       <MenuItem label="Notes" to="/Notes" />
