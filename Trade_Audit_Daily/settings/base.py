f03f7111049198ba926fc907aa0ed95c4ed5e494
rushikesh <rushikesh.raut@trigensoft.com>
2024-01-07 16:29:26 +0530

    new migrations on trade_app

--- Trade_Audit_Daily/settings/base.py
+++ Trade_Audit_Daily/settings/base.py
@@ -119,14 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = [ # Here
-    BASE_DIR / 'core/static/'
-]
+
 
 
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
