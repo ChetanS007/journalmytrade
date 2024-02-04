d2b08f1b8e4e326de2d7affeb62c8b010552e503
rushikesh <rushikesh.raut@trigensoft.com>
2024-02-04 21:32:51 +0530

    cors middleware on top

--- Trade_Audit_Daily/settings/base.py
+++ Trade_Audit_Daily/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'Trade_Audit_Daily.urls'
